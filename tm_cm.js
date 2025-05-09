6012361b7484174248006c7f5982b9fe0c8b1405
Archos62 <jf1989@msn.com>
2025-05-09 10:26:44 +0200

    MAJ tm_cm.js
    Ajout des blocs bronze, argent, or et vide

--- tm_cm.js
+++ tm_cm.js
@@ -135,7 +135,11 @@ var blockList = [
   {putUnder: "resources", text: "Prelude", src:"card_prelude", otherbg:"res_otherbg"},
   {putUnder: "resources", text: "Corpo", src:"card_corpo", otherbg:"res_otherbg"},
   {putUnder: "tiles", text: "", src:"colony_plate"},
-  {putUnder: "tiles", text: "Standard project", src:"Standard_tile"}
+  {putUnder: "tiles", text: "Standard project", src:"Standard_tile"},
+  {putUnder: "resources", text: "Bronze", src:"bronze", otherbg:"res_otherbg"},
+  {putUnder: "resources", text: "Silver", src:"silver", otherbg:"res_otherbg"},
+  {putUnder: "resources", text: "Gold", src:"gold", otherbg:"res_otherbg"},
+  {putUnder: "resources", text: "Blank", src:"blank", otherbg:"res_otherbg"}
 ];
 
 var blockDefaults = {
